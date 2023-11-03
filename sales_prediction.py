c391cc534dcb40ab1bd982d11f57065dbefa7503
nurshakirahmdsalleh <125841212+nurshakirahmdsalleh@users.noreply.github.com>
2023-11-03 11:05:54 +0800

    Create sales_prediction.py

---
+++ sales_prediction.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+import pickle
+import sklearn
+from sklearn import preprocessing
+
+loaded_model = pickle.load(open("sales-advertising-model.h5","rb"))
+scaler_features = pickle.load(open("features-scaler.pkl","rb"))
+scaler_sales = pickle.load(open("sales-scaler.pkl","rb"))
