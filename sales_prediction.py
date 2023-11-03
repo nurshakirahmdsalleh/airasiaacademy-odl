cecbc141b3724d3d750a30e07e9417f87258d6b7
nurshakirahmdsalleh <125841212+nurshakirahmdsalleh@users.noreply.github.com>
2023-11-03 11:13:07 +0800

    Update sales_prediction.py

--- sales_prediction.py
+++ sales_prediction.py
@@ -7,3 +7,6 @@ from sklearn import preprocessing
 loaded_model = pickle.load(open("sales-advertising-model.h5","rb"))
 scaler_features = pickle.load(open("features-scaler.pkl","rb"))
 scaler_sales = pickle.load(open("sales-scaler.pkl","rb"))
+
+st.title("Sales Prediction App")
+st.write("This app predicts the sales based on three advertising channel features")
