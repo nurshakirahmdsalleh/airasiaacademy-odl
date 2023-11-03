8bd90e5bb30be7250d2d0108d39ee17ab4f4e6bf
nurshakirahmdsalleh <125841212+nurshakirahmdsalleh@users.noreply.github.com>
2023-11-03 11:19:52 +0800

    Update sales_prediction.py

--- sales_prediction.py
+++ sales_prediction.py
@@ -10,3 +10,17 @@ scaler_sales = pickle.load(open("sales-scaler.pkl","rb"))
 
 st.title("Sales Prediction App")
 st.write("This app predicts the sales based on three advertising channel features")
+
+st.sidebar.header("User Input Parameters")
+
+def user_input_features():
+  tv = st.sidebar.slider('TV',0,300,0)
+  radio = st.sidebar.slider('Radio',0,50,0)
+  newspaper = st.sidebar.slider('Newspaper',0,50,0)
+  data = {'TV':tv,
+          'Radio':radio,
+          'Newspaper':newspaper}
+  features = pd.DataFrame(data, index=[0])
+  return features
+
+df = user_input_features()
